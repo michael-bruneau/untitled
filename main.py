dd706cf89d9820390a74902520e9d2713b093a15
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-11-27 19:04:36 -0500

    annotations

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+""" 
+Copyright (c) 2020 MTHS All rights reserved
+Created by: Michael Bruneau
+Created on: Nov 2023
+This program drives car and stops before hitting wall
+"""
+
 import robotbit
 import sonar
 
+# setup
 basic.show_icon(IconNames.HAPPY)
 
+# loop forever
 while True:
     if input.button_is_pressed(Button.A):
         while True:
+            # checking distance
             distance = sonar.ping(
                 DigitalPin.P1, 
                 DigitalPin.P2, 
@@ -13,9 +23,13 @@ while True:
             )
             basic.show_number(distance)
             basic.show_string("cm")
+
+            # if distance if 11 or greater move stepper moters 10 cm forward
             if distance >= 11:
                 robotbit.stp_car_move(10, 42)
                 basic.pause(500)
+
+                # if stepper motor is 10 cm or less than 10 cm turn stepper motors 10 cm backward & turn 90 deegres
             else:
                 robotbit.stp_car_move(-10, 42)
                 basic.pause(500)
--- main.ts
+++ main.ts
@@ -1,15 +1,27 @@
 let distance: number;
+/**  
+Copyright (c) 2020 MTHS All rights reserved
+Created by: Michael Bruneau
+Created on: Nov 2023
+This program drives car and stops before hitting wall
+
+ */
+//  setup
 basic.showIcon(IconNames.Happy)
+//  loop forever
 while (true) {
     if (input.buttonIsPressed(Button.A)) {
         while (true) {
+            //  checking distance
             distance = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Centimeters)
             basic.showNumber(distance)
             basic.showString("cm")
+            //  if distance if 11 or greater move stepper moters 10 cm forward
             if (distance >= 11) {
                 robotbit.StpCarMove(10, 42)
                 basic.pause(500)
             } else {
+                //  if stepper motor is 10 cm or less than 10 cm turn stepper motors 10 cm backward & turn 90 deegres
                 robotbit.StpCarMove(-10, 42)
                 basic.pause(500)
                 robotbit.StpCarTurn(90, 42, 125)
